f94f72e7d409d8dc3dc7009c81c32103e8027c9e
rdashnox <lr.rrnocum@mmdc.mcl.edu.ph>
2026-09-17 17:22:42 +0800

    [FR-09] Documents: add StudentDocumentsPage component

---
+++ src/pages/StudentDocumentsPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { StudentDocumentsView } from "../features/documents";
+import useAuth from "../hooks/useAuth";
+import { useStudents } from "../features/students/hooks/useStudents";
+
+export default function StudentDocumentsPage() {
+  const { user } = useAuth();
+  const { data: studentProfile } = useStudents(user?.role);
+
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h4" fontWeight={700} gutterBottom>
+        My Documents
+      </Typography>
+      {studentProfile?.currentInternship?.id ? (
+        <StudentDocumentsView internshipId={studentProfile.currentInternship.id} />
+      ) : (
+        <Typography>No active internship found.</Typography>
+      )}
+    </Box>
+  );
+}
